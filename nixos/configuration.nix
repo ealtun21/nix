bd81b605d48ee573d750bc82b1bbd0d1556168c0
Eray Altun <nyverin@gmail.com>
2024-08-29 19:41:19 +0300

    enabled udisk2

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,6 +60,8 @@
     };
   };
 
+  services.udisks2.enable = true;
+
   programs.bash = {
     interactiveShellInit = ''
       if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
